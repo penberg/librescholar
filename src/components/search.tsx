0aad2fb5620d405b2b50903997f6122603a1b6b6
Pekka Enberg <penberg@iki.fi>
2023-12-17 17:02:29 +0200

    Bring back dark mode

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,5 +1,3 @@
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
 import OpenAI from 'openai';
 import { search as searchGoogleScholar } from "scholarly";
 
@@ -35,8 +33,8 @@ export default async function Search({ question }: { question: string }) {
     <div className="container mx-auto px-4 py-6 flex flex-col">
       <h1 className="text-3xl font-bold mb-8 text-center">LibreScholar</h1>
       <form className="flex w-full max-w-xl items-center space-x-2 mb-8 justify-center" action="/">
-        <Input className="w-full" placeholder="Enter a research question" name="question" type="text" defaultValue={question} />
-        <Button type="submit">Search</Button>
+        <input className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter a research question" name="question" type="text" defaultValue={question} />
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
       </form>
       { searchTerms }
       <ul className="list-disc list-inside mb-6">
