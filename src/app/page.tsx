ff222ed7f10613b1d77fdc769128c75bfd04b085
Pekka Enberg <penberg@iki.fi>
2023-12-17 13:40:13 +0200

    Parse research question using OpenAI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Search } from '@/components/search'
+import Search from '@/components/search'
 
-export default function Home() {
+export default function Home({ params, searchParams }: { params: any, searchParams: any }) {
+  const question = searchParams.question;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Search></Search>
+        <Search question={question}></Search>
     </main>
   )
 }
