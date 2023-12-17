4926d35d3e18438eee03099a2e1d4c45678f6d08
Pekka Enberg <penberg@iki.fi>
2023-12-17 17:49:06 +0200

    Fix Github button

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -33,7 +33,7 @@ export default async function Search({ question }: { question: string }) {
     <div className="container mx-auto px-4 py-6 flex flex-col">
       <div className="flex items-center justify-between mb-8">
       <h1 className="text-3xl font-bold text-center mx-auto">LibreScholar</h1>
-      <Button className="border-black border rounded-lg font-semibold py-1 px-4  ml-4 bg-black text-white"><a href="https://github.com/penberg/librescholar">Github</a></Button>
+      <button className="border-black border rounded-lg font-semibold py-1 px-4  ml-4 bg-black text-white"><a href="https://github.com/penberg/librescholar">Github</a></button>
       </div>
       <form className="flex w-full max-w-xl items-center space-x-2 mb-8 justify-center" action="/">
         <input className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter a research question" name="question" type="text" defaultValue={question} />
