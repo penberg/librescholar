50c6275807fb84297ed4d97a13cff6e676c97e38
Pekka Enberg <penberg@iki.fi>
2023-12-17 16:09:19 +0200

    Show search terms

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -8,21 +8,29 @@ const openai = new OpenAI({
 });
 
 export default async function Search({ question }: { question: string }) {
-  const search = async (question: string): Promise<any[]> => {
+  const search = async (question: string): Promise<[string, any[]]> => {
     'use server'
     if (!question) {
-      return [];
+      return ["", []];
     }
     const prompt = `Please generate a list of keywords to use on Google Scholar to find relevant papers for the following research question: "${question}" The output must be a list of words, separated by spaces, but nothing more.`;
     const completion = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       messages: [{ role: 'user', content: prompt }],
     });
-    const keywords = completion.choices[0]?.message?.content;
+    const keywords = completion.choices[0]?.message?.content!;
     const results = await searchGoogleScholar(keywords!);
-    return results;
+    return [keywords, results];
   }
-  const results = await search(question);
+  const [keywords, results] = await search(question);
+  var searchTerms = null;
+  if (question) {
+    searchTerms = (
+      <p className="mb-6">
+        <b>Search terms:</b> {keywords}
+      </p>
+    )
+  }
   return (
     <div className="container mx-auto px-4 py-6 flex flex-col">
       <h1 className="text-3xl font-bold mb-8 text-center">LibreScholar</h1>
@@ -30,6 +38,7 @@ export default async function Search({ question }: { question: string }) {
         <Input className="w-full" placeholder="Enter a research question" name="question" type="text" />
         <Button type="submit">Search</Button>
       </form>
+      { searchTerms }
       <ul className="list-disc list-inside mb-6">
       {
         results.map((result: any) => (
